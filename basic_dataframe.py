4fe2114de15a8cec39d7a2627928e39f55ed0ec6
Dewansh01 <dewansh0109@gmail.com>
2025-06-23 14:04:55 +0530

    Create basic_dataframe.py

---
+++ basic_dataframe.py
@@ -0,0 +1,10 @@
+dem07 = [{"a" :np.nan,"b" :2,"c" :np.nan,"d": 4},{"a" :1,"b" :2,"c" :3,"d": 4},{"b" :2,"d": 4},{"a" :1,"b" :2,"c" :3,"d": 4}]
+pd.DataFrame(dem07)
+# np.nan ------- is used to create empty space
+
+# Output
+# a	b	c	d
+# 0	NaN	2	NaN	4
+# 1	1.0	2	3.0	4
+# 2	NaN	2	NaN	4
+# 3	1.0	2	3.0	4
